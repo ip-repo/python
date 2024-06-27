74cfd06d23c44236963a8f82e172fe061c8a74f2
ip-repo <123945379+ip-repo@users.noreply.github.com>
2024-06-27 11:12:46 +0300

    Create sentiment-analysis.py

---
+++ streamlit-scripts/sentiment-analysis.py
@@ -0,0 +1,25 @@
+from transformers import pipeline
+import streamlit as st
+
+#Load model
+sentiment_analyzer = pipeline("sentiment-analysis", model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
+
+
+st.title("Sentiment Analysis Chatbot")
+user_input = st.text_input("Enter your message:")
+
+if user_input:
+# Perform sentiment analysis
+    sentiment = sentiment_analyzer(user_input)[0]
+    sentiment_label = sentiment["label"]
+    sentiment_score = sentiment["score"]
+
+    # Display bot response based on sentiment
+    if sentiment_label == "POSITIVE":
+        st.success("That sounds great! 😄  \n" +"Score: " + str(sentiment_score))
+    else:
+        st.error("I'm sorry to hear that. 😔  \n" +"Score: " + str(sentiment_score))
+
+
+
+
